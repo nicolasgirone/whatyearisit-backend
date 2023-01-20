713dc011a6c6f4ff25e432a1fe0d43de66436922
Your name <Your email>
2023-01-20 10:41:41 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET year. */
+router.get('/year', function(req, res, next) {
+  res.send('date', { year: '2050' });
+});
+
+module.exports = router;
